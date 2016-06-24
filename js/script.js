fc344f397872485681a75b050e60d815c99e10c7
Dylan Conkle <dconkle@cytracom.com>
2016-06-23 21:09:52 -0500

    Fixed Regex for pricing instead of email/quantity instead of password/item instead of username

--- js/script.js
+++ js/script.js
@@ -2,11 +2,13 @@ $(function() {
     var dialog, form,
 
         // From http://www.whatwg.org/specs/web-apps/current-work/multipage/states-of-the-type-attribute.html#e-mail-state-%28type=email%29
-        emailRegex = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
+        nameRegex = /^[a-z]([0-9a-z_\s])+$/i,
+        priceRegex = /^\d+(?:[.]\d{1,2}|$)$/,
+        quantityRegex = /^([0-9a-zA-Z])+$/,
         name = $("#name"),
-        email = $("#email"),
-        password = $("#password"),
-        allFields = $([]).add(name).add(email).add(password),
+        price = $("#price"),
+        quantity = $("#quantity"),
+        allFields = $([]).add(name).add(price).add(quantity),
         tips = $(".validateTips");
 
     function updateTips(t) {
@@ -43,19 +45,19 @@ $(function() {
         var valid = true;
         allFields.removeClass("ui-state-error");
 
-        valid = valid && checkLength(name, "username", 3, 16);
-        valid = valid && checkLength(email, "email", 6, 80);
-        valid = valid && checkLength(password, "password", 5, 16);
+        valid = valid && checkLength(name, "username", 1, 16);
+        valid = valid && checkLength(price, "price", 1, 80);
+        valid = valid && checkLength(quantity, "quantity", 1, 16);
 
-        valid = valid && checkRegexp(name, /^[a-z]([0-9a-z_\s])+$/i, "Username may consist of a-z, 0-9, underscores, spaces and must begin with a letter.");
-        valid = valid && checkRegexp(email, emailRegex, "eg. ui@jquery.com");
-        valid = valid && checkRegexp(password, /^([0-9a-zA-Z])+$/, "Password field only allow : a-z 0-9");
+        valid = valid && checkRegexp(name, nameRegex, "Item Name may consist of a-z, 0-9, underscores, spaces and must begin with a letter.");
+        valid = valid && checkRegexp(price, priceRegex, "eg. 1.23");
+        valid = valid && checkRegexp(quantity, quantityRegex, "Please enter a Valid Number");
 
         if (valid) {
             $("#users tbody").append("<tr>" +
                 "<td>" + name.val() + "</td>" +
-                "<td>" + email.val() + "</td>" +
-                "<td>" + password.val() + "</td>" +
+                "<td>" + price.val() + "</td>" +
+                "<td>" + quantity.val() + "</td>" +
                 "</tr>");
             dialog.dialog("close");
         }
@@ -68,7 +70,7 @@ $(function() {
         width: 350,
         modal: true,
         buttons: {
-            "Create an account": addUser,
+            "Add Item to Shopping list": addUser,
             Cancel: function() {
                 dialog.dialog("close");
             }
