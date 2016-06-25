c2b739e84e1beb06718085269a5cc0dd9fd2566b
Dylan Conkle <dconkle@cytracom.com>
2016-06-25 14:16:01 -0500

    Cleaned up shopping list js file

--- js/script.js
+++ js/script.js
@@ -12,46 +12,41 @@ $(function() {
         tips = $(".validateTips");
 
     function updateTips(t) {
-        tips
-            .text(t)
-            .addClass("ui-state-highlight");
+        tips.text(t).addClass("ui-state-highlight");
+
         setTimeout(function() {
-            tips.removeClass("ui-state-highlight", 1500);
+            tips.removeClass("ui-state-highlight");
         }, 500);
     }
 
     function checkLength(o, n, min, max) {
-        if (o.val().length > max || o.val().length < min) {
-            o.addClass("ui-state-error");
-            updateTips("Length of " + n + " must be between " +
-                min + " and " + max + ".");
-            return false;
-        } else {
+        if (o.val().length <= max && o.val().length >= min)
             return true;
-        }
+
+        o.addClass("ui-state-error");
+        updateTips("Length of " + n + " must be between " + min + " and " + max + ".");
+        return false;
     }
 
     function checkRegexp(o, regexp, n) {
-        if (!(regexp.test(o.val()))) {
-            o.addClass("ui-state-error");
-            updateTips(n);
-            return false;
-        } else {
+        if (regexp.test(o.val()))
             return true;
-        }
+
+        o.addClass("ui-state-error");
+        updateTips(n);
+        return false;
     }
 
     function addUser() {
         var valid = true;
         allFields.removeClass("ui-state-error");
 
-        valid = valid && checkLength(name, "username", 1, 16);
-        valid = valid && checkLength(price, "price", 1, 80);
-        valid = valid && checkLength(quantity, "quantity", 1, 16);
-
-        valid = valid && checkRegexp(name, nameRegex, "Item Name may consist of a-z, 0-9, underscores, spaces and must begin with a letter.");
-        valid = valid && checkRegexp(price, priceRegex, "eg. 1.23");
-        valid = valid && checkRegexp(quantity, quantityRegex, "Please enter a Valid Number");
+        valid = valid && checkLength(name, "username", 1, 16)
+                      && checkLength(price, "price", 1, 80)
+                      && checkLength(quantity, "quantity", 1, 16)
+                      && checkRegexp(name, nameRegex, "Item Name may consist of a-z, 0-9, underscores, spaces and must begin with a letter.")
+                      && checkRegexp(price, priceRegex, "eg. 1.23")
+                      && checkRegexp(quantity, quantityRegex, "Please enter a Valid Number");
 
         if (valid) {
             $("#users tbody").append("<tr>" +
@@ -65,22 +60,24 @@ $(function() {
         return valid;
     }
 
-    dialog = $("#dialog-form").dialog({
-        autoOpen: false,
-        height: 300,
-        width: 350,
-        modal: true,
-        buttons: {
-            "Add Item to Shopping list": addUser,
-            Cancel: function() {
-                dialog.dialog("close");
+    dialog = $("#dialog-form").dialog(
+        {
+            autoOpen: false,
+            height: 300,
+            width: 350,
+            modal: true,
+            buttons: {
+                "Add Item to Shopping list": addUser,
+                Cancel: function() {
+                    dialog.dialog("close");
+                }
+            },
+            close: function() {
+                form[0].reset();
+                allFields.removeClass("ui-state-error");
             }
-        },
-        close: function() {
-            form[0].reset();
-            allFields.removeClass("ui-state-error");
         }
-    });
+    );
 
     form = dialog.find("form").on("submit", function(event) {
         event.preventDefault();
@@ -91,8 +88,9 @@ $(function() {
         dialog.dialog("open");
     });
 });
+
 $(document).ready(function() {
-            $('#shoppinglist').on('click', '.removebtn', function() {
-                $(this).closest('tr').remove();
-            });
-  });
+    $('#shoppinglist').on('click', '.removebtn', function() {
+        $(this).closest('tr').remove();
+    });
+});
